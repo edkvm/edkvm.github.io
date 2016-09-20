5444256850c3ef7e3d852b58fc448159a4d1c8e8
klimslava <eddy.kiselman@gmail.com>
2016-09-20 08:53:34 +0300

    Adjust scale

--- js/vis.js
+++ js/vis.js
@@ -41,7 +41,7 @@
       this.currentSelection = null;
       
       this.fill_color = d3.scale.linear()
-        .domain([1, 7])
+        .domain([1, 3, 5, 13])
         .range(["#BDC9E1","#74A9CF", "#2B8CBE", "#045A8D"]);
       
       max_amount = d3.max(this.data, function(d) {
